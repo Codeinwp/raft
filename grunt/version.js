8159b9c7d03efeef6dedaeed0d2e80dccee2d540
andreibaicus <baicusandrei@me.com>
2022-08-02 15:38:34 +0300

    ci: adds semantic-release & deploy workflows

--- grunt/version.js
+++ grunt/version.js
@@ -3,20 +3,20 @@
 module.exports = {
 	package: {
 		options: {
-			prefix: '"version"\\:\\s+"'
+			prefix: '"version"\\:\\s+"',
 		},
-		src: 'package.json'
+		src: 'package.json',
 	},
 	stylesheet: {
 		options: {
-			prefix: 'Version\\:\\s+'
+			prefix: 'Version\\:\\s+',
 		},
-		src: 'style.css'
+		src: 'style.css',
 	},
 	functions: {
 		options: {
-			prefix: 'RRAFT_VERSION\', \''
+			prefix: "RAFT_VERSION', '",
 		},
-		src: 'functions.php'
-	}
+		src: 'functions.php',
+	},
 };
