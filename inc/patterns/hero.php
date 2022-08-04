907af04fe6588531b8bc9ca71bbdf7be0831c90d
andreibaicus <baicusandrei@me.com>
2022-08-04 10:33:02 +0300

    chore: front page template structure

--- inc/patterns/hero.php
+++ inc/patterns/hero.php
@@ -26,37 +26,39 @@ return array(
 	'title'      => __( 'Hero section', 'raft' ),
 	'categories' => array( 'raft' ),
 	'content'    => '
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"64px","bottom":"64px"},"blockGap":"24px","margin":{"top":"0px","bottom":"0px"}}},"backgroundColor":"raft-bg-alt","layout":{"inherit":true}} -->
-<div class="wp-block-group alignfull has-raft-bg-alt-background-color has-background" style="margin-top:0px;margin-bottom:0px;padding-top:64px;padding-bottom:64px">
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"64px","bottom":"64px"},"blockGap":"24px","margin":{"top":"0px","bottom":"0px"}}},"backgroundColor":"raft-bg-alt","layout":{"inherit":true}} -->
+<div class="wp-block-group has-raft-bg-alt-background-color has-background" style="margin-top:0px;margin-bottom:0px;padding-top:64px;padding-bottom:64px"><!-- wp:heading {"textAlign":"center","fontSize":"huge"} -->
 
-<!-- wp:heading {"textAlign":"center","level":1,"fontSize":"xxl"} -->
-<h1 class="has-text-align-center has-xxl-font-size">' . esc_html( $strings['title'] ) . '</h1>
+<!-- wp:heading {"textAlign":"center","level":1,"fontSize":"huge"} -->
+<h1 class="has-text-align-center has-huge-font-size">' . esc_html( $strings['title'] ) . '</h1>
 <!-- /wp:heading -->
 
-    <!-- wp:paragraph {"align":"center","fontSize":"medium"} -->
-    <p class="has-text-align-center has-medium-font-size">' . esc_html( $strings['subtitle'] ) . '</p>
-    <!-- /wp:paragraph -->
+<!-- wp:paragraph {"align":"center","fontSize":"medium"} -->
+<p class="has-text-align-center has-medium-font-size">' . esc_html( $strings['subtitle'] ) . '</p>
+<!-- /wp:paragraph -->
 
-    <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
-    <div class="wp-block-buttons">
-        <!-- wp:button {"textColor":"raft-fg-alt"} -->
-        <div class="wp-block-button">
-        	<a class="wp-block-button__link has-raft-fg-alt-color has-text-color">' . esc_html( $strings['button'] ) . '</a>
-        </div>
-        <!-- /wp:button -->
-    </div>
-    <!-- /wp:buttons -->
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons">
 
-    <!-- wp:spacer {"height":"3vw"} -->
-    <div style="height:3vw" aria-hidden="true" class="wp-block-spacer"></div>
-    <!-- /wp:spacer -->
+<!-- wp:button {"textColor":"raft-fg-alt"} -->
+<div class="wp-block-button">
+<a class="wp-block-button__link has-raft-fg-alt-color has-text-color">' . esc_html( $strings['button'] ) . '</a>
+</div>
+<!-- /wp:button -->
 
-    <!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
-    <figure class="wp-block-image alignwide size-full">
-    	<img src="' . esc_url( $hero_illustration ) . '" alt="Hero Illustration"/>
-    </figure>
-    <!-- /wp:image -->
-    
+</div>
+<!-- /wp:buttons -->
+
+<!-- wp:spacer {"height":"3vw"} -->
+<div style="height:3vw" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:image {"align":"center","sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-large">
+<img src="' . esc_url( $hero_illustration ) . '" alt="Hero Illustration"/>
+</figure>
+<!-- /wp:image -->
+
 </div>
 <!-- /wp:group -->
 ',
---
+++ inc/patterns/three-columns-features.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Pattern
+ *
+ * @author Themeisle
+ * @package raft
+ * @since 1.0.0
+ *
+ * slug: images-text-columns
+ * title: Two columns of features
+ * categories: raft
+ * keywords: section, image, text, columns, title
+ */
+
+$columns = array(
+	array(
+		'image' => 'shape-05.svg',
+		'title' => __( 'Style Variations', 'raft' ),
+	),
+	array(
+		'image' => 'shape-06.svg',
+		'title' => __( 'Built-in Patterns', 'raft' ),
+	),
+	array(
+		'image' => 'shape-04.svg',
+		'title' => __( 'Powered by blocks', 'raft' ),
+	),
+);
+
+$strings = array(
+	'title' => __( 'Three columns of features', 'raft' ),
+);
+
+$columns_markup = '';
+
+foreach ( $columns as $column ) {
+	$columns_markup .= '<!-- wp:column -->';
+	$columns_markup .= '<div class="wp-block-column">';
+
+	$columns_markup .= '<!-- wp:image {"sizeSlug":"large","linkDestination":"none"} -->';
+	$columns_markup .= '<figure class="wp-block-image size-full">';
+	$columns_markup .= '<img src="' . esc_url( RAFT_URL . 'assets/img/' . $column['image'] ) . '" alt="Illustration"/>';
+	$columns_markup .= '</figure>';
+	$columns_markup .= '<!-- /wp:image -->';
+
+	$columns_markup .= '<!-- wp:heading {"level":3} -->';
+	$columns_markup .= '<h3>' . esc_html( $column['title'] ) . '</h3>';
+	$columns_markup .= '<!-- /wp:heading -->';
+
+	$columns_markup .= '<!-- wp:paragraph -->';
+	$columns_markup .= '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>';
+	$columns_markup .= '<!-- /wp:paragraph -->';
+
+	$columns_markup .= '</div>';
+	$columns_markup .= '<!-- /wp:column -->';
+}
+
+return array(
+	'title'      => $strings['title'],
+	'categories' => array( 'raft' ),
+	'content'    => '
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"64px","bottom":"64px"},"margin":{"top":"0px","bottom":"0px"}}},"layout":{"inherit":false,"wideSize":"960px"}} -->
+<div class="wp-block-group alignfull" style="margin-top:0px;margin-bottom:0px;padding-top:64px;padding-bottom:64px">
+
+<!-- wp:group -->
+<div class="wp-block-group">
+
+<!-- wp:columns -->
+<div class="wp-block-columns">' . wp_kses_post( $columns_markup ) . '</div>
+<!-- /wp:columns -->
+
+</div>
+<!-- /wp:group -->
+
+</div>
+<!-- /wp:group -->
+',
+);
