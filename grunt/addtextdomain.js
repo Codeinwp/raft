895b3a9de6ba2be06022d4ead56bfbfb5ea0019a
andreibaicus <baicusandrei@me.com>
2022-08-11 11:46:45 +0300

    chore: fix addtextdomain task [skip ci]

--- grunt/addtextdomain.js
+++ grunt/addtextdomain.js
@@ -1,27 +1,22 @@
 /* jshint node:true */
 // https://github.com/blazersix/grunt-wp-i18n
 module.exports = {
-  theme: {
-    options: {
-      textdomain: '<%= package.theme.textdomain %>'
-    },
-    theme: {
-      files: {
-        src: [
-          '<%= files.php %>'
-        ]
-      }
-    }
-  },
-  composer: {
-    options: {
-      textdomain: '<%= package.theme.textdomain %>',
-      updateDomains: ['textdomain']
-    },
-    files: {
-      src: [
-        'vendor/codeinwp/**/*.php'
-      ]
-    }
-  }
-}
+	theme: {
+		options: {
+			updateDomains: true,
+			textdomain: '<%= package.theme.textdomain %>',
+		},
+		files: {
+			src: [ '<%= files.php %>' ],
+		},
+	},
+	composer: {
+		options: {
+			textdomain: '<%= package.theme.textdomain %>',
+			updateDomains: true,
+		},
+		files: {
+			src: [ 'vendor/codeinwp/**/*.php' ],
+		},
+	},
+};
