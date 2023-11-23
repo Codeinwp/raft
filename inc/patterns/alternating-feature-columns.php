fdb01c1da55c45fa91340ebd6b3ae3ddb4d3032a
John Fraskos <jfraskos@gmail.com>
2023-11-23 16:02:47 +0200

    Update alternating-feature-columns.php

--- inc/patterns/alternating-feature-columns.php
+++ inc/patterns/alternating-feature-columns.php
@@ -25,7 +25,7 @@ $images = array(
 
 return array(
 	'title'      => __( 'Alternating feature columns', 'raft' ),
-	'categories' => array( 'raft' ),
+	'categories' => array( 'raft/features' ),
 	'content'    => '
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"0px","bottom":"0px","right":"0px","left":"0px"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"0px"}},"layout":{"inherit":false}} -->
 <div class="wp-block-group alignfull" style="margin-top:0px;margin-bottom:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:columns {"verticalAlignment":"center","style":{"spacing":{"blockGap":"0px"}}} -->
