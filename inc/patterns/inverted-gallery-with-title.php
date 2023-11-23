04787feb2aa355d53e556da84a5294ca6880b098
John Fraskos <jfraskos@gmail.com>
2023-11-23 16:17:53 +0200

    Update inverted-gallery-with-title.php

--- inc/patterns/inverted-gallery-with-title.php
+++ inc/patterns/inverted-gallery-with-title.php
@@ -27,7 +27,7 @@ $images = array(
 
 return array(
 	'title'      => $strings['title'],
-	'categories' => array( 'raft' ),
+	'categories' => array( 'raft/content' ),
 	'content'    => '
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"64px","bottom":"64px"},"margin":{"top":"0px","bottom":"0px"}}},"backgroundColor":"raft-fg","textColor":"raft-bg","layout":{"inherit":true,"type":"constrained"},"otterConditions":[]} -->
 <div class="wp-block-group alignfull has-raft-bg-color has-raft-fg-background-color has-text-color has-background" style="margin-top:0px;margin-bottom:0px;padding-top:64px;padding-bottom:64px"><!-- wp:group {"align":"wide","layout":{"inherit":false}} -->
