e4e3981427ae7f92fc5ce8bb7331447f67248a80
John Fraskos <jfraskos@gmail.com>
2023-12-05 19:31:43 +0200

    added testimonial-background

--- inc/patterns/testimonial-background.php
+++ inc/patterns/testimonial-background.php
@@ -8,7 +8,7 @@
  *
  * slug: testimonial-background
  * title: Testimonial on Background
- * categories: raft/content
+ * categories: raft/testimonials
  * keywords: testimonial, feedback
  */
 
@@ -20,7 +20,7 @@ $images = array(
 
 return array(
 	'title'      => __( 'Testimonial on Background', 'raft' ),
-	'categories' => array( 'raft/content' ),
+	'categories' => array( 'raft/testimonials' ),
 	'content'    => '
 		<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"var:preset|spacing|40","right":"var:preset|spacing|40"},"blockGap":"var:preset|spacing|80","margin":{"top":"0","bottom":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|raft-fg-alt"}}}},"backgroundColor":"raft-accent","textColor":"raft-fg-alt","layout":{"type":"constrained"},"metadata":{}} -->
 		<div class="wp-block-group alignfull has-raft-fg-alt-color has-raft-accent-background-color has-text-color has-background has-link-color" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--40)">
