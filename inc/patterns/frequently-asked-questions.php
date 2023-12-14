f829f9a380c257eb42ca2d3e007406681fad958c
John Fraskos <jfraskos@gmail.com>
2023-12-14 14:18:20 +0200

    Update frequently-asked-questions.php

--- inc/patterns/frequently-asked-questions.php
+++ inc/patterns/frequently-asked-questions.php
@@ -1,81 +1,69 @@
 <?php
 /**
- * Pattern
+ * Frequently Asked Questions Pattern for Raft Theme
  *
  * @author Themeisle
  * @package raft
- * @since 1.0.0
+ * @since 1.0.5
  *
- * slug: faq-section
- * title: Frequently asked questions
- * categories: raft
- * keywords: section, faq, frequently, asked, boxes
+ * slug: frequently-asked-questions
+ * title: Frequently Asked Questions
+ * categories: raft/content
+ * keywords: FAQ, questions, answers
  */
 
-$column_markup = '
-<!-- wp:column {"style":{"spacing":{"padding":{"top":"24px","right":"24px","bottom":"24px","left":"24px"}}},"backgroundColor":"raft-bg-alt"} -->
-<div class="wp-block-column has-raft-bg-alt-background-color has-background" style="padding-top:24px;padding-right:24px;padding-bottom:24px;padding-left:24px">
-
-<!-- wp:heading {"level":3,"fontSize":"medium"} -->
-<h3 class="has-medium-font-size">Lorem ipsum sit dolor</h3>
-<!-- /wp:heading -->
-
-<!-- wp:paragraph -->
-<p>Ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-<!-- /wp:paragraph -->
-
-</div>
-<!-- /wp:column -->
-';
-
-$strings = array(
-	'title'  => __( 'Frequently asked questions', 'raft' ),
-	'byline' => __( 'Have questions? We have the answers!', 'raft' ),
-	'button' => __( 'See More FAQs', 'raft' ),
-);
+$raft_strings = apply_filters( 'raft_strings', array() );
 
 return array(
-	'title'      => $strings['title'],
-	'categories' => array( 'raft/content' ),
-	'content'    => '
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"64px","bottom":"64px"},"margin":{"top":"0px","bottom":"0px"}}},"layout":{"inherit":true}} -->
-<div class="wp-block-group alignfull" style="margin-top:0px;margin-bottom:0px;padding-top:64px;padding-bottom:64px">
-<!-- wp:heading {"textAlign":"center","className":"is-style-default"} -->
-<h2 class="has-text-align-center is-style-default">' . esc_html( $strings['title'] ) . '</h2>
-<!-- /wp:heading -->
+    'title'      => __( 'Frequently Asked Questions', 'raft' ),
+    'categories' => array( 'raft/content' ),
+    'content'    => '
+        <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"var:preset|spacing|40","right":"var:preset|spacing|40"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"var:preset|spacing|40"}},"layout":{"inherit":true,"type":"constrained"}} -->
+        <div class="wp-block-group alignfull" style="margin-top:0px;margin-bottom:0px;padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--40)"><!-- wp:heading {"textAlign":"center","className":"is-style-default"} -->
+        <h2 class="wp-block-heading has-text-align-center is-style-default">' . esc_html( $raft_strings['faq_section_title'] ) . '</h2>
+        <!-- /wp:heading -->
 
-<!-- wp:paragraph {"align":"center","fontSize":"normal"} -->
-<p class="has-text-align-center has-normal-font-size">' . esc_html( $strings['byline'] ) . '</p>
-<!-- /wp:paragraph -->
+        <!-- wp:paragraph {"align":"center","fontSize":"normal"} -->
+        <p class="has-text-align-center has-normal-font-size">' . esc_html( $raft_strings['section_description'] ) . '</p>
+        <!-- /wp:paragraph -->
 
-<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
-<div class="wp-block-buttons">
-<!-- wp:button {"textColor":"raft-fg-alt"} -->
-<div class="wp-block-button">
-<a class="wp-block-button__link has-raft-fg-alt-color has-text-color">' . esc_html( $strings['button'] ) . '</a>
-</div>
-<!-- /wp:button -->
-</div>
-<!-- /wp:buttons -->
+        <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+        <div class="wp-block-buttons"><!-- wp:button {"textColor":"raft-fg-alt"} -->
+        <div class="wp-block-button"><a class="wp-block-button__link has-raft-fg-alt-color has-text-color wp-element-button">' . esc_html( $raft_strings['button_text'] ) . '</a></div>
+        <!-- /wp:button --></div>
+        <!-- /wp:buttons -->
 
-<!-- wp:spacer {"height":"40px"} -->
-<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer -->
+        <!-- wp:spacer {"height":"40px"} -->
+        <div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+        <!-- /wp:spacer -->
 
-<!-- wp:group {"align":"wide"} -->
-<div class="wp-block-group alignwide">
+        <!-- wp:group {"align":"wide"} -->
+        <div class="wp-block-group alignwide"><!-- wp:columns -->
+        <div class="wp-block-columns">
+        <div class="wp-block-column has-raft-bg-alt-background-color has-background" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
+            <h3 class="wp-block-heading has-medium-font-size">' . esc_html( $raft_strings['faq_title'] ) . '</h3>
+            <p>' . esc_html( $raft_strings['paragraph_text'] ) . '</p>
+        </div>
+        <div class="wp-block-column has-raft-bg-alt-background-color has-background" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
+            <h3 class="wp-block-heading has-medium-font-size">' . esc_html( $raft_strings['faq_title'] ) . '</h3>
+            <p>' . esc_html( $raft_strings['paragraph_text'] ) . '</p>
+        </div>
+        </div>
+        <!-- /wp:columns -->
 
-<!-- wp:columns -->
-<div class="wp-block-columns">' . wp_kses_post( $column_markup . $column_markup ) . '</div>
-<!-- /wp:columns -->
-
-<!-- wp:columns -->
-<div class="wp-block-columns">' . wp_kses_post( $column_markup . $column_markup ) . '</div>
-<!-- /wp:columns -->
-
-</div>
-<!-- /wp:group -->
-
-</div>
-<!-- /wp:group -->',
+        <!-- wp:columns -->
+        <div class="wp-block-columns">
+        <div class="wp-block-column has-raft-bg-alt-background-color has-background" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
+            <h3 class="wp-block-heading has-medium-font-size">' . esc_html( $raft_strings['faq_title'] ) . '</h3>
+            <p>' . esc_html( $raft_strings['paragraph_text'] ) . '</p>
+        </div>
+        <div class="wp-block-column has-raft-bg-alt-background-color has-background" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
+            <h3 class="wp-block-heading has-medium-font-size">' . esc_html( $raft_strings['faq_title'] ) . '</h3>
+            <p>' . esc_html( $raft_strings['paragraph_text'] ) . '</p>
+        </div>
+        </div>
+        <!-- /wp:columns --></div>
+        <!-- /wp:group --></div>
+        <!-- /wp:group -->
+    ',
 );
