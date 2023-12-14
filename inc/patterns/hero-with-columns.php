eac1f86d9ce3407cd087466c7f36c394c092002a
John Fraskos <jfraskos@gmail.com>
2023-12-14 14:19:04 +0200

    Update hero-with-columns.php

--- inc/patterns/hero-with-columns.php
+++ inc/patterns/hero-with-columns.php
@@ -1,115 +1,69 @@
 <?php
 /**
- * Pattern
+ * Hero with Columns Pattern for Raft Theme
  *
  * @author Themeisle
  * @package raft
  * @since 1.0.5
  *
  * slug: hero-with-columns
- * title: Hero section with columns
- * categories: raft
- * keywords: section, hero, columns
+ * title: Hero with Columns
+ * categories: raft/heroes_page_titles
+ * keywords: hero, columns, features
  */
 
-$strings = array(
-	'title'      => __( 'Hero section with columns', 'raft' ),
-	'learn_more' => __( 'Learn More', 'raft' ),
+$raft_strings = apply_filters( 'raft_strings', array() );
+
+$images = array(
+    RAFT_URL . 'assets/img/shape-05.svg',
 );
 
-$image = RAFT_URL . 'assets/img/shape-05.svg';
-
-
 return array(
-	'title'      => $strings['title'],
-	'categories' => array( 'raft/heroes_page_titles' ),
-	'content'    => '
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"64px","bottom":"64px"},"blockGap":"32px","margin":{"top":"0px","bottom":"0px"}}},"layout":{"inherit":true,"type":"constrained"},"otterConditions":[]} -->
-<div class="wp-block-group alignfull" style="margin-top:0px;margin-bottom:0px;padding-top:64px;padding-bottom:64px"><!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"32px"}},"otterConditions":[]} -->
-<div class="wp-block-group alignwide"><!-- wp:spacer {"height":"64px"} -->
-<div style="height:64px" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer -->
+    'title'      => __( 'Hero with Columns', 'raft' ),
+    'categories' => array( 'raft/heroes_page_titles' ),
+    'content'    => '
+        <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"var:preset|spacing|40","right":"var:preset|spacing|40"},"margin":{"top":"0","bottom":"0"},"blockGap":"var:preset|spacing|50"}},"backgroundColor":"raft-bg-alt","layout":{"inherit":true,"type":"constrained"}} -->
+        <div class="wp-block-group alignfull has-raft-bg-alt-background-color has-background" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--40)"><!-- wp:group {"layout":{"inherit":false}} -->
+        <div class="wp-block-group"><!-- wp:heading {"textAlign":"center","fontSize":"huge"} -->
+        <h2 class="wp-block-heading has-text-align-center has-huge-font-size">' . esc_html( $raft_strings['hero_title'] ) . '</h2>
+        <!-- /wp:heading -->
 
-<!-- wp:heading {"textAlign":"center","level":1,"fontSize":"xxl"} -->
-<h1 class="has-text-align-center has-xxl-font-size">' . esc_html( $strings['title'] ) . '</h1>
-<!-- /wp:heading -->
+        <!-- wp:paragraph {"align":"center","fontSize":"large"} -->
+        <p class="has-text-align-center has-large-font-size">' . esc_html( $raft_strings['section_description'] ) . '</p>
+        <!-- /wp:paragraph -->
 
-<!-- wp:paragraph {"align":"center","fontSize":"medium"} -->
-<p class="has-text-align-center has-medium-font-size">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
-<!-- /wp:paragraph -->
+        <!-- wp:spacer {"height":"40px"} -->
+        <div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+        <!-- /wp:spacer --></div>
+        <!-- /wp:group -->
 
-<!-- wp:separator {"backgroundColor":"raft-accent"} -->
-<hr class="wp-block-separator has-text-color has-raft-accent-color has-alpha-channel-opacity has-raft-accent-background-color has-background"/>
-<!-- /wp:separator -->
+        <!-- wp:group {"align":"wide","layout":{"inherit":false}} -->
+        <div class="wp-block-group alignwide"><!-- wp:columns -->
+        <div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"blockGap":"var:preset|spacing|40"}}} -->
+        <div class="wp-block-column is-vertically-aligned-center"><!-- wp:heading {"level":3} -->
+        <h3 class="wp-block-heading">' . esc_html( $raft_strings['feature_title'] ) . '</h3>
+        <!-- /wp:heading -->
 
-<!-- wp:columns -->
-<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"top","style":{"spacing":{"padding":{"top":"24px","right":"24px","bottom":"24px","left":"24px"}}},"backgroundColor":"raft-bg-alt"} -->
-<div class="wp-block-column is-vertically-aligned-top has-raft-bg-alt-background-color has-background" style="padding-top:24px;padding-right:24px;padding-bottom:24px;padding-left:24px">
-<!-- wp:image {"width":150,"height":150,"sizeSlug":"full","linkDestination":"none","className":"is-style-rounded"} -->
-<figure class="wp-block-image size-full is-resized is-style-rounded"><img src="' . esc_url( $image ) . '" alt="" width="150" height="150"/></figure>
-<!-- /wp:image -->
+        <!-- wp:paragraph -->
+        <p>' . esc_html( $raft_strings['feature_description'] ) . '</p>
+        <!-- /wp:paragraph -->
 
-<!-- wp:heading {"fontSize":"medium"} -->
-<h2 class="has-medium-font-size">Lorem ipsum</h2>
-<!-- /wp:heading -->
+        <!-- wp:heading {"level":3} -->
+        <h3 class="wp-block-heading">' . esc_html( $raft_strings['feature_title'] ) . '</h3>
+        <!-- /wp:heading -->
 
-<!-- wp:paragraph -->
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing.</p>
-<!-- /wp:paragraph -->
+        <!-- wp:paragraph -->
+        <p>' . esc_html( $raft_strings['feature_description'] ) . '</p>
+        <!-- /wp:paragraph --></div>
+        <!-- /wp:column -->
 
-<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"space-between"}} -->
-<div class="wp-block-buttons"><!-- wp:button {"style":{"spacing":{"padding":{"top":"8px","right":"16px","bottom":"8px","left":"16px"}}},"className":"is-style-fill","otterConditions":[]} -->
-<div class="wp-block-button is-style-fill"><a class="wp-block-button__link wp-element-button" style="padding-top:8px;padding-right:16px;padding-bottom:8px;padding-left:16px">' . esc_html( $strings['learn_more'] ) . '</a></div>
-<!-- /wp:button --></div>
-<!-- /wp:buttons --></div>
-<!-- /wp:column -->
-
-<!-- wp:column {"verticalAlignment":"top","style":{"spacing":{"padding":{"top":"24px","right":"24px","bottom":"24px","left":"24px"}}},"backgroundColor":"raft-bg-alt"} -->
-<div class="wp-block-column is-vertically-aligned-top has-raft-bg-alt-background-color has-background" style="padding-top:24px;padding-right:24px;padding-bottom:24px;padding-left:24px">
-<!-- wp:image {"width":150,"height":150,"sizeSlug":"full","linkDestination":"none","className":"is-style-rounded"} -->
-<figure class="wp-block-image size-full is-resized is-style-rounded"><img src="' . esc_url( $image ) . '" alt="" width="150" height="150"/></figure>
-<!-- /wp:image -->
-
-<!-- wp:heading {"fontSize":"medium"} -->
-<h2 class="has-medium-font-size">Lorem ipsum</h2>
-<!-- /wp:heading -->
-
-<!-- wp:paragraph -->
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing. sed do eiusmod tempor incididunt ut labore et dolore.</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"space-between"}} -->
-<div class="wp-block-buttons"><!-- wp:button {"style":{"spacing":{"padding":{"top":"8px","right":"16px","bottom":"8px","left":"16px"}}},"className":"is-style-fill","otterConditions":[]} -->
-<div class="wp-block-button is-style-fill"><a class="wp-block-button__link wp-element-button" style="padding-top:8px;padding-right:16px;padding-bottom:8px;padding-left:16px">' . esc_html( $strings['learn_more'] ) . '</a></div>
-<!-- /wp:button --></div>
-<!-- /wp:buttons --></div>
-<!-- /wp:column -->
-
-<!-- wp:column {"verticalAlignment":"top","style":{"spacing":{"padding":{"top":"24px","right":"24px","bottom":"24px","left":"24px"}}},"backgroundColor":"raft-bg-alt"} -->
-<div class="wp-block-column is-vertically-aligned-top has-raft-bg-alt-background-color has-background" style="padding-top:24px;padding-right:24px;padding-bottom:24px;padding-left:24px">
-<!-- wp:image {"width":150,"height":150,"sizeSlug":"full","linkDestination":"none","className":"is-style-rounded"} -->
-<figure class="wp-block-image size-full is-resized is-style-rounded"><img src="' . esc_url( $image ) . '" alt="" width="150" height="150"/></figure>
-<!-- /wp:image -->
-
-<!-- wp:heading {"fontSize":"medium"} -->
-<h2 class="has-medium-font-size">Lorem ipsum</h2>
-<!-- /wp:heading -->
-
-<!-- wp:paragraph -->
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing.</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"space-between"}} -->
-<div class="wp-block-buttons"><!-- wp:button {"style":{"spacing":{"padding":{"top":"8px","right":"16px","bottom":"8px","left":"16px"}}},"className":"is-style-fill","otterConditions":[]} -->
-<div class="wp-block-button is-style-fill"><a class="wp-block-button__link wp-element-button" style="padding-top:8px;padding-right:16px;padding-bottom:8px;padding-left:16px">' . esc_html( $strings['learn_more'] ) . '</a></div>
-<!-- /wp:button --></div>
-<!-- /wp:buttons --></div>
-<!-- /wp:column --></div>
-<!-- /wp:columns -->
-
-<!-- wp:spacer {"height":"64px"} -->
-<div style="height:64px" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer --></div>
-<!-- /wp:group --></div>
-<!-- /wp:group -->',
+        <!-- wp:column -->
+        <div class="wp-block-column"><!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+        <figure class="wp-block-image size-full"><img src="' . esc_url( $images[0] ) . '" alt=""/></figure>
+        <!-- /wp:image --></div>
+        <!-- /wp:column --></div>
+        <!-- /wp:columns --></div>
+        <!-- /wp:group --></div>
+        <!-- /wp:group -->
+    ',
 );
