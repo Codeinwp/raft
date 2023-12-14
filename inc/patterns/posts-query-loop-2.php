1f13c9db7fdf9903e1fdb624f77393413a9d2a84
John Fraskos <jfraskos@gmail.com>
2023-12-14 14:47:51 +0200

    Update posts-query-loop-2.php

--- inc/patterns/posts-query-loop-2.php
+++ inc/patterns/posts-query-loop-2.php
@@ -1,51 +1,44 @@
 <?php
 /**
- * Pattern
+ * Posts Query Loop 2 Pattern for Raft Theme
  *
  * @author Themeisle
  * @package raft
  * @since 1.0.5
  *
  * slug: posts-query-loop-2
- * title: Posts Query loop 2
- * categories: raft
- * keywords: section, blog, posts, latest
+ * title: Posts Query Loop 2
+ * categories: raft/post_loops
+ * keywords: posts, query, loop, grid
  */
 
-$strings = array(
-	'title'    => __( 'Posts Query loop', 'raft' ) . ' 2',
-	'no_posts' => __( 'Unfortunately no posts were found', 'raft' ),
-);
+$raft_strings = apply_filters( 'raft_strings', array() );
 
 return array(
-	'title'      => $strings['title'],
-	'categories' => array( 'raft/post_loops' ),
-	'content'    => '
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"64px","bottom":"64px"},"margin":{"top":"0px","bottom":"0px"}}},"layout":{"inherit":true,"type":"constrained"},"otterConditions":[]} -->
-<div class="wp-block-group alignfull" style="margin-top:0px;margin-bottom:0px;padding-top:64px;padding-bottom:64px"><!-- wp:query {"queryId":3,"query":{"perPage":"4","pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false},"displayLayout":{"type":"flex","columns":2},"align":"wide"} -->
-<div class="wp-block-query alignwide"><!-- wp:post-template -->
-<!-- wp:post-featured-image {"height":"","align":"center"} /-->
+    'title'      => __( 'Posts Query Loop 2', 'raft' ),
+    'categories' => array( 'raft/post_loops' ),
+    'content'    => '
+        <div class="wp-block-group alignfull" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--40)">
+        <div class="wp-block-query alignwide">
+            <!-- wp:post-template -->
+            <div class="wp-block-group" style="margin-top:0;margin-bottom:0">
+                <!-- wp:post-featured-image {"height":"","align":"center"} /-->
+                <!-- wp:post-date {"style":{"typography":{"textTransform":"uppercase"}},"fontSize":"small"} /-->
+                <!-- wp:post-title /-->
+            </div>
+            <!-- /wp:post-template -->
 
-<!-- wp:post-date {"style":{"typography":{"textTransform":"uppercase"}},"fontSize":"small"} /-->
+            <div style="height:80px" aria-hidden="true" class="wp-block-spacer"></div>
 
-<!-- wp:post-title /-->
-<!-- /wp:post-template -->
+            <!-- wp:query-pagination {"layout":{"type":"flex","justifyContent":"space-between"}} -->
+            <!-- wp:query-pagination-previous /-->
+            <!-- wp:query-pagination-next /-->
+            <!-- /wp:query-pagination -->
 
-<!-- wp:spacer {"height":"80px"} -->
-<div style="height:80px" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer -->
-
-<!-- wp:query-pagination {"layout":{"type":"flex","justifyContent":"space-between"}} -->
-<!-- wp:query-pagination-previous /-->
-
-<!-- wp:query-pagination-next /-->
-<!-- /wp:query-pagination -->
-
-<!-- wp:query-no-results -->
-<!-- wp:paragraph {"backgroundColor":"raft-bg-alt"} -->
-<p class="has-raft-bg-alt-background-color has-background">' . esc_html( $strings['no_posts'] ) . '</p>
-<!-- /wp:paragraph -->
-<!-- /wp:query-no-results --></div>
-<!-- /wp:query --></div>
-<!-- /wp:group -->',
+            <!-- wp:query-no-results -->
+            <p class="has-raft-bg-alt-background-color has-background">' . esc_html( $raft_strings['no_posts_found'] ) . '</p>
+            <!-- /wp:query-no-results -->
+        </div>
+        </div>
+    ',
 );
