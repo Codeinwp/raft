4bd827abcafd3954fdfdecfab848d3cd1df30314
John Fraskos <jfraskos@gmail.com>
2023-12-14 15:22:23 +0200

    Update two-columns-features.php
    fixed syntax

--- inc/patterns/two-columns-features.php
+++ inc/patterns/two-columns-features.php
@@ -23,25 +23,60 @@ return array(
     'title'      => __( 'Two Columns of Features', 'raft' ),
     'categories' => array( 'raft/features' ),
     'content'    => '
+        <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"var:preset|spacing|40","right":"var:preset|spacing|40"},"margin":{"top":"0","bottom":"0"},"blockGap":"var:preset|spacing|60"}},"backgroundColor":"raft-bg-alt","layout":{"inherit":true,"type":"constrained"}} -->
         <div class="wp-block-group alignfull has-raft-bg-alt-background-color has-background" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--40)">
-        <div class="wp-block-group">
-        <h2 class="wp-block-heading">' . esc_html( $raft_strings['feature_section_title'] ) . '</h2>
-        <p class="has-medium-font-size">' . esc_html( $raft_strings['section_description'] ) . '</p>
+            <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"constrained"}} -->
+            <div class="wp-block-group">
+                <!-- wp:heading -->
+                <h2 class="wp-block-heading">' . esc_html( $raft_strings['feature_section_title'] ) . '</h2>
+                <!-- /wp:heading -->
+
+                <!-- wp:paragraph {"fontSize":"medium"} -->
+                <p class="has-medium-font-size">' . esc_html( $raft_strings['section_description'] ) . '</p>
+                <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+
+            <!-- wp:group -->
+            <div class="wp-block-group">
+                <!-- wp:columns -->
+                <div class="wp-block-columns">
+                    <!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|40"}}} -->
+                    <div class="wp-block-column">
+                        <!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+                        <figure class="wp-block-image size-full"><img src="' . esc_url( $images[0] ) . '" alt="Illustration"/></figure>
+                        <!-- /wp:image -->
+
+                        <!-- wp:heading {"level":3} -->
+                        <h3 class="wp-block-heading">' . esc_html( $raft_strings['feature_title'] ) . '</h3>
+                        <!-- /wp:heading -->
+
+                        <!-- wp:paragraph -->
+                        <p>' . esc_html( $raft_strings['feature_description'] ) . '</p>
+                        <!-- /wp:paragraph -->
+                    </div>
+                    <!-- /wp:column -->
+
+                    <!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|40"}}} -->
+                    <div class="wp-block-column">
+                        <!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+                        <figure class="wp-block-image size-full"><img src="' . esc_url( $images[1] ) . '" alt="Illustration"/></figure>
+                        <!-- /wp:image -->
+
+                        <!-- wp:heading {"level":3} -->
+                        <h3 class="wp-block-heading">' . esc_html( $raft_strings['feature_title'] ) . '</h3>
+                        <!-- /wp:heading -->
+
+                        <!-- wp:paragraph -->
+                        <p>' . esc_html( $raft_strings['feature_description'] ) . '</p>
+                        <!-- /wp:paragraph -->
+                    </div>
+                    <!-- /wp:column -->
+                </div>
+                <!-- /wp:columns -->
+            </div>
+            <!-- /wp:group -->
         </div>
-        <div class="wp-block-group">
-        <div class="wp-block-columns">
-        <div class="wp-block-column">
-        <figure class="wp-block-image size-full"><img src="' . esc_url( $images[0] ) . '" alt="Illustration"/></figure>
-        <h3 class="wp-block-heading">' . esc_html( $raft_strings['feature_title'] ) . '</h3>
-        <p>' . esc_html( $raft_strings['feature_description'] ) . '</p>
-        </div>
-        <div class="wp-block-column">
-        <figure class="wp-block-image size-full"><img src="' . esc_url( $images[1] ) . '" alt="Illustration"/></figure>
-        <h3 class="wp-block-heading">' . esc_html( $raft_strings['feature_title'] ) . '</h3>
-        <p>' . esc_html( $raft_strings['feature_description'] ) . '</p>
-        </div>
-        </div>
-        </div>
-        </div>
+        <!-- /wp:group -->
     ',
 );
