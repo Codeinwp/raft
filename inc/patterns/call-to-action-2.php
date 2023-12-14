7c179c8b9fb65af6ef45bb67daf20a6d7d61bc5c
John Fraskos <jfraskos@gmail.com>
2023-12-14 14:16:54 +0200

    Update call-to-action-2.php

--- inc/patterns/call-to-action-2.php
+++ inc/patterns/call-to-action-2.php
@@ -1,42 +1,45 @@
 <?php
 /**
- * Pattern
+ * Call to Action Pattern
  *
  * @author Themeisle
  * @package raft
  * @since 1.0.5
  *
- * slug: call-to-action-2
- * title: Call to action 2
- * categories: raft
- * keywords: section, cta, button
+ * slug: call-to-action-pattern
+ * title: Call to Action 2
+ * categories: raft/call_to_action
+ * keywords: call to action, CTA, engagement
  */
 
+$raft_strings = apply_filters( 'raft_strings', array() );
+
 return array(
-	'title'      => __( 'Call to action', 'raft' ) . ' 2',
-	'categories' => array( 'raft/call_to_action' ),
-	'content'    => '
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"64px","bottom":"64px"},"margin":{"top":"0px","bottom":"0px"}}},"layout":{"inherit":true,"type":"constrained"},"otterConditions":[]} -->
-<div class="wp-block-group alignfull" style="margin-top:0px;margin-bottom:0px;padding-top:64px;padding-bottom:64px"><!-- wp:group {"align":"wide","layout":{"inherit":false}} -->
-<div class="wp-block-group alignwide"><!-- wp:columns -->
-<div class="wp-block-columns"><!-- wp:column {"width":"50%"} -->
-<div class="wp-block-column" style="flex-basis:50%"><!-- wp:heading {"fontSize":"huge"} -->
-<h2 class="has-huge-font-size">Lorem ipsum sit dolor</h2>
-<!-- /wp:heading --></div>
-<!-- /wp:column -->
+    'title'      => __( 'Call to Action 2', 'raft' ),
+    'categories' => array( 'raft/call_to_action' ),
+    'content'    => '
+        <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"var:preset|spacing|40","right":"var:preset|spacing|40"},"margin":{"top":"0","bottom":"0"},"blockGap":"0"}},"layout":{"inherit":true,"type":"constrained"}} -->
+        <div class="wp-block-group alignfull" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--40)"><!-- wp:group {"align":"wide","layout":{"inherit":false}} -->
+        <div class="wp-block-group alignwide"><!-- wp:columns -->
+        <div class="wp-block-columns"><!-- wp:column {"width":"50%"} -->
+        <div class="wp-block-column" style="flex-basis:50%"><!-- wp:heading -->
+        <h2 class="wp-block-heading">' . esc_html( $raft_strings['cta_title'] ) . '</h2>
+        <!-- /wp:heading --></div>
+        <!-- /wp:column -->
 
-<!-- wp:column {"width":"50%"} -->
-<div class="wp-block-column" style="flex-basis:50%"><!-- wp:paragraph {"align":"left","fontSize":"large"} -->
-<p class="has-text-align-left has-large-font-size">Ed ut perspiciatis unde omnis iste natus error sit voluptatem.</p>
-<!-- /wp:paragraph -->
+        <!-- wp:column {"width":"50%"} -->
+        <div class="wp-block-column" style="flex-basis:50%"><!-- wp:paragraph {"align":"left"} -->
+        <p class="has-text-align-left">' . esc_html( $raft_strings['feature_description'] ) . '</p>
+        <!-- /wp:paragraph -->
 
-<!-- wp:buttons {"layout":{"type":"flex"}} -->
-<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-primary is-style-fill","otterConditions":[]} -->
-<div class="wp-block-button is-style-primary is-style-fill"><a class="wp-block-button__link wp-element-button">' . esc_html__( 'Learn More', 'raft' ) . '</a></div>
-<!-- /wp:button --></div>
-<!-- /wp:buttons --></div>
-<!-- /wp:column --></div>
-<!-- /wp:columns --></div>
-<!-- /wp:group --></div>
-<!-- /wp:group -->',
+        <!-- wp:buttons {"layout":{"type":"flex"}} -->
+        <div class="wp-block-buttons"><!-- wp:button {"className":"is-style-primary is-style-fill"} -->
+        <div class="wp-block-button is-style-primary is-style-fill"><a class="wp-block-button__link wp-element-button">' . esc_html( $raft_strings['button_text'] ) . '</a></div>
+        <!-- /wp:button --></div>
+        <!-- /wp:buttons --></div>
+        <!-- /wp:column --></div>
+        <!-- /wp:columns --></div>
+        <!-- /wp:group --></div>
+        <!-- /wp:group -->
+    ',
 );
