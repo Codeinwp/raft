1e8dfad9fc74576eff4350e2db2e3a76ee9778f6
John Fraskos <jfraskos@gmail.com>
2023-12-14 14:47:12 +0200

    Update posts-query-loop.php

--- inc/patterns/posts-query-loop.php
+++ inc/patterns/posts-query-loop.php
@@ -1,69 +1,46 @@
 <?php
 /**
- * Pattern
+ * Posts Query Loop Pattern for Raft Theme
  *
  * @author Themeisle
  * @package raft
- * @since 1.0.0
+ * @since 1.0.5
  *
  * slug: posts-query-loop
- * title: Posts Query loop
- * categories: raft
- * keywords: section, blog, posts, latest
+ * title: Posts Query Loop
+ * categories: raft/post_loops
+ * keywords: posts, query, loop
  */
 
-$strings = array(
-	'button'   => __( 'Go to post', 'raft' ),
-	'title'    => __( 'Posts Query loop', 'raft' ),
-	'no_posts' => __( 'Unfortunately no posts were found', 'raft' ),
-);
+$raft_strings = apply_filters( 'raft_strings', array() );
 
 return array(
-	'title'      => $strings['title'],
-	'categories' => array( 'raft/post_loops' ),
-	'content'    => '
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"64px","bottom":"64px"},"margin":{"top":"0px","bottom":"0px"}}},"layout":{"inherit":true}} -->
-<div class="wp-block-group alignfull" style="margin-top:0px;margin-bottom:0px;padding-top:64px;padding-bottom:64px">
-<!-- wp:heading -->
-<h2>' . esc_html( $strings['title'] ) . '</h2>
-<!-- /wp:heading -->
+    'title'      => __( 'Posts Query Loop', 'raft' ),
+    'categories' => array( 'raft/post_loops' ),
+    'content'    => '
+        <div class="wp-block-group alignfull" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--40)">
+        <div class="wp-block-query">
+        <div class="wp-block-group">
+            <!-- wp:post-template -->
+            <!-- wp:post-featured-image {"align":"center"} /-->
+            <!-- wp:post-date {"fontSize":"small"} /-->
+            <!-- wp:post-title /-->
+            <!-- wp:post-terms {"term":"category","fontSize":"small"} /-->
+            <!-- wp:post-excerpt {"moreText":"' . esc_html( $raft_strings['continue_reading'] ) . '","style":{"elements":{"link":{"color":{"text":"var:preset|color|raft-accent"}}}}} /-->
+            <div style="height:64px" aria-hidden="true" class="wp-block-spacer"></div>
+            <!-- /wp:post-template -->
 
-<!-- wp:spacer {"height":"40px"} -->
-<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer -->
+            <!-- wp:query-pagination {"layout":{"type":"flex","justifyContent":"space-between"}} -->
+            <!-- wp:query-pagination-previous /-->
+            <!-- wp:query-pagination-numbers /-->
+            <!-- wp:query-pagination-next /-->
+            <!-- /wp:query-pagination -->
 
-<!-- wp:query {"query":{"perPage":"1","pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false}} -->
-<div class="wp-block-query">
-
-<!-- wp:post-template -->
-
-<!-- wp:post-featured-image {"align":"center"} /-->
-<!-- wp:post-date {"fontSize":"small"} /-->
-<!-- wp:post-title /-->
-<!-- wp:post-terms {"term":"category","fontSize":"small"} /-->
-<!-- wp:post-excerpt {"moreText":"\u003cstrong\u003e' . esc_html( $strings['button'] ) . '\u003c/strong\u003e","style":{"elements":{"link":{"color":{"text":"var:preset|color|raft-accent"}}}}} /-->
-
-<!-- wp:spacer {"height":"64px"} -->
-<div style="height:64px" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer -->
-
-<!-- /wp:post-template -->
-
-<!-- wp:query-pagination {"layout":{"type":"flex","justifyContent":"space-between"}} -->
-<!-- wp:query-pagination-previous /-->
-<!-- wp:query-pagination-numbers /-->
-<!-- wp:query-pagination-next /-->
-<!-- /wp:query-pagination -->
-
-<!-- wp:query-no-results -->
-<!-- wp:paragraph {"backgroundColor":"raft-bg-alt"} -->
-<p class="has-raft-bg-alt-background-color has-background">Unfortuna' . esc_html( $strings['no_posts'] ) . 'd</p>
-<!-- /wp:paragraph -->
-<!-- /wp:query-no-results -->
-
-</div>
-<!-- /wp:query -->
-
-</div>
-<!-- /wp:group -->',
+            <!-- wp:query-no-results -->
+            <p class="has-raft-bg-alt-background-color has-background">' . esc_html( $raft_strings['no_posts_found'] ) . '</p>
+            <!-- /wp:query-no-results -->
+        </div>
+        </div>
+        </div>
+    ',
 );
